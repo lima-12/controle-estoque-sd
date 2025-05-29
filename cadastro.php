f9f75e33015234fb8849d0ebe7f7a08d669ba053
Deydson Costa <103591633+falloutdey@users.noreply.github.com>
2025-05-29 18:50:54 -0300

    tela de cadastro

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stok - Login</title>
+    <title>Stok - Cadastro</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="src/assets/css/style2.css">
@@ -13,45 +13,56 @@
     <div class="login-container">
         <div class="login-card ">
             <div class="d-flex flex-column align-items-center text-center mb-2">
-            <!-- Logo -->
-            <img src="src/assets/img/stok azul e laranja.png" alt="Logo Stok" class="login-logo w-50">
             <!-- Títulos -->
-            <h1 class="login-title">Bem-vindo de volta</h1>
-            <p class="login-subtitle">Entre na sua conta para continuar</p>
+            <h1 class="login-title-cadastro">Crie sua conta</h1>
             </div>
 
             <!-- Formulário -->
             <form>
-                <!-- Campo de Email -->
+                <!-- Campo de Nome-->
+                <div class="input-group-custom">
+                    <i class="fa-solid fa-user input-icon-custom"></i>
+                    <input class="form-control form-control-custom" placeholder="Nome" required>
+                </div>
+
                 <div class="input-group-custom">
                     <i class="fas fa-envelope input-icon-custom"></i>
                     <input type="email" class="form-control form-control-custom" placeholder="seu@email.com" required>
                 </div>
                 
-                <!-- Campo de Senha -->
+                <!-- Campo Senha Principal -->
                 <div class="input-group-custom">
                     <i class="fas fa-lock input-icon-custom"></i>
-                    <input type="password" class="form-control form-control-custom" placeholder="********" required>
+                    <input type="password" class="form-control form-control-custom password-input" placeholder="********" required>
                     <button type="button" class="password-toggle">
                         <i class="fas fa-eye"></i>
                     </button>
                 </div>
 
-                <!-- Botão de Entrar -->
+                <!-- Campo Confirmação de Senha -->
+                <div class="input-group-custom">
+                    <i class="fas fa-lock input-icon-custom"></i>
+                    <input type="password" class="form-control form-control-custom password-input" placeholder="********" required>
+                    <button type="button" class="password-toggle">
+                        <i class="fas fa-eye"></i>
+                    </button>
+                </div>
+
+                <!-- Botão de Criar -->
                 <button type="submit" class="btn btn-entrar">
-                    Entrar
+                    Criar
                 </button>
 
                 <!-- Rodapé -->
                 <div class="login-footer">
-                    <span class="texto-abaixo">Não tem uma conta?</span>
-                    <a href="cadastro.php" class="link-orange">Cadastre-se</a>
+                    <span class="texto-abaixo">Já possuí uma conta?</span>
+                    <a href="index2.php" class="link-orange">Entre agora mesmo</a>
                 </div>
             </form>
         </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="src/assets/js/script2.js"></script>
+    <script src="src\assets\js\cadastro.js"></script>
 </body>
 </html>
--- src/assets/js/script2.js
+++ src/assets/js/script2.js
@@ -1,15 +1,16 @@
-
-document.querySelector('.password-toggle').addEventListener('click', function() {
-    const passwordInput = this.previousElementSibling;
-    const icon = this.querySelector('i');
-    
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        icon.classList.replace('fa-eye', 'fa-eye-slash');
-    } else {
-        passwordInput.type = 'password';
-        icon.classList.replace('fa-eye-slash', 'fa-eye');
-    }
+document.querySelectorAll('.password-toggle').forEach(toggle => {
+    toggle.addEventListener('click', function() {
+        const passwordInput = this.previousElementSibling;
+        const icon = this.querySelector('i');
+        
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            icon.classList.replace('fa-eye', 'fa-eye-slash');
+        } else {
+            passwordInput.type = 'password';
+            icon.classList.replace('fa-eye-slash', 'fa-eye');
+        }
+    });
 });
 
 // Simulate loading on form submit
