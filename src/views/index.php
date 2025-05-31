7ce84826e4cda40749a56c93260df02c61732c96
lima-12 <limaufpa2022@gmail.com>
2025-05-31 19:15:44 -0300

    ajuste: caminho do arquivo js

--- src/views/index.php
+++ src/views/index.php
@@ -52,6 +52,6 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="src/assets/js/script2.js"></script>
+    <script src="../assets/js/script2.js"></script>
 </body>
 </html>
