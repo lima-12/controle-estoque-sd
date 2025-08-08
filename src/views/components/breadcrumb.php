984a5ec6146ee6b7394de9c9b9023c8988498ac0
lima-12 <limaufpa2022@gmail.com>
2025-08-08 18:31:18 -0300

    melhora componente breadcrumb

--- src/views/components/breadcrumb.php
+++ src/views/components/breadcrumb.php
@@ -21,7 +21,7 @@
     }
 ?>
 
-<nav aria-label="breadcrumb" class="bg-light border-bottom">
+<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
     <div class="container py-2">
         <ol class="breadcrumb mb-0">
             <?php foreach ($breadcrumbs as $index => $item): ?>
