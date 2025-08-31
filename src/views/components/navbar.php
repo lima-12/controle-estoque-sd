aed54073af981e6ff20e006c65ef55d8b247f8eb
lima-12 <limaufpa2022@gmail.com>
2025-08-31 16:02:25 -0300

    ajuste navbar

--- src/views/components/navbar.php
+++ src/views/components/navbar.php
@@ -1,124 +1,45 @@
 <?php
 require_once __DIR__ . '/../../config/Session.php';
+require_once __DIR__ . '/../../config/Config.php';
 
-$currentScript = $_SERVER['SCRIPT_NAME'];
-$basePath = '';
-
-if (strpos($currentScript, '/home.php') !== false) {
-    $basePath = '../assets/';
-} elseif (strpos($currentScript, '/produto/') !== false) {
-    $basePath = '../../assets/';
-} elseif (strpos($currentScript, '/filiais/') !== false) {
-    $basePath = '../../assets/';
-} elseif (strpos($currentScript, '/usuarios/') !== false) {
-    $basePath = '../../assets/';
-} else {
-    $basePath = '../assets/';
-}
-
-// Obtém o usuário logado
 $usuarioLogado = Session::getUser();
-
-// Calcula os caminhos corretos para os links baseado na página atual
-$homePath = '';
-$produtosPath = '';
-$relatoriosPath = '';
-$usuariosPath = '';
-
-// Se estamos na página home.php
-if (strpos($currentScript, '/home.php') !== false) {
-    $homePath = './home.php';
-    $produtosPath = './produto/index.php';
-    $relatoriosPath = './relatorios/index.php';
-    $usuariosPath = './usuarios/index.php';
-}
-// Se estamos em qualquer página dentro da pasta produto/
-elseif (strpos($currentScript, '/produto/') !== false) {
-    $homePath = '../home.php';
-    $produtosPath = './index.php';
-    $relatoriosPath = '../relatorios/index.php';
-    $usuariosPath = '../usuarios/index.php';
-}
-// Se estamos em qualquer página dentro da pasta filiais/
-elseif (strpos($currentScript, '/filiais/') !== false) {
-    $homePath = '../home.php';
-    $produtosPath = '../produto/index.php';
-    $relatoriosPath = '../relatorios/index.php';
-    $usuariosPath = '../usuarios/index.php';
-}
-// Se estamos em qualquer página dentro da pasta usuarios/
-elseif (strpos($currentScript, '/usuarios/') !== false) {
-    $homePath = '../home.php';
-    $produtosPath = '../produto/index.php';
-    $relatoriosPath = '../relatorios/index.php';
-    $usuariosPath = './index.php';
-}
-// Para outras páginas na raiz de views/
-else {
-    $homePath = './home.php';
-    $produtosPath = './produto/index.php';
-    $relatoriosPath = './relatorios/index.php';
-    $usuariosPath = './usuarios/index.php';
-}
 ?>
 
-<nav class="navbar-expand-lg navbar bg-primary">
+<nav class="navbar navbar-expand-lg bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?php echo $homePath; ?>">
-            <img src="<?php echo $basePath; ?>img/logo-stok-azul-laranja.png" alt="Logo Stok" class="navbar-logo"/>
+        <!-- Logo -->
+        <a class="navbar-brand" href="<?= url('home.php') ?>">
+            <img src="<?= asset('img/logo-stok-azul-laranja.png') ?>" alt="Logo Stok" class="navbar-logo"/>
         </a>
         
-        <!-- Botão de alternância para abrir o menu offcanvas -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <!-- Botão toggle mobile -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
         
-        <!-- Menu desktop -->
-        <!-- <div class="navbar-nav ms-auto">
-            <div class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fas fa-user me-1"></i><?= htmlspecialchars($usuarioLogado['nome']) ?>
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="../../controllers/sair.php">
-                        <i class="fas fa-sign-out-alt me-2"></i>Sair
-                    </a></li>
-                </ul>
-            </div>
-        </div> -->
-        
-        <!-- Menu offcanvas que aparece em telas menores -->
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <!-- Cabeçalho do menu offcanvas com título ou logotipo -->
+        <!-- Offcanvas -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
-                    <img src="<?php echo $basePath; ?>img/logo-stok-azul-laranja.png" alt="Logo Stok" class="navbar-logo"/>
+                <h5 class="offcanvas-title">
+                    <img src="<?= asset('img/logo-stok-azul-laranja.png') ?>" alt="Logo Stok" class="navbar-logo"/>
                 </h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
             </div>
-            <!-- Corpo do menu offcanvas com links de navegação -->
+            
             <div class="offcanvas-body">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="<?php echo $homePath; ?>">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="<?php echo $produtosPath; ?>">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="<?php echo $usuariosPath; ?>">Usuários</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="<?php echo $relatoriosPath; ?>">Relatórios</a>
-                    </li>
+                <ul class="navbar-nav ms-auto pe-3">
+                    <li class="nav-item"><a class="nav-link" href="<?= url('home.php') ?>">Início</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= url('produto/index.php') ?>">Produtos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= url('usuarios/index.php') ?>">Usuários</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= url('relatorios/index.php') ?>">Relatórios</a></li>
                     
-                    <!-- Dropdown do usuário -->
+                    <!-- Dropdown usuário -->
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
                             <i class="fas fa-user me-1"></i><?= htmlspecialchars($usuarioLogado['nome']) ?>
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="../controllers/sair.php">
+                            <li><a class="dropdown-item" href="<?= BASE_URL ?>controllers/sair.php">
                                 <i class="fas fa-sign-out-alt me-2"></i>Sair
                             </a></li>
                         </ul>
@@ -126,6 +47,5 @@ else {
                 </ul>
             </div>
         </div>
-
     </div>
 </nav>
