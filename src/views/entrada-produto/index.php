942242aa6d1ee59df7baf68a14fef5805f671ab4
lima-12 <limaufpa2022@gmail.com>
2025-09-01 16:12:25 -0300

    ajuste entrada produtos

--- src/views/entrada-produto/index.php
+++ src/views/entrada-produto/index.php
@@ -16,6 +16,9 @@ Session::requireLogin();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <!-- Estilos personalizados -->
     <link rel="stylesheet" href="../../assets/css/entrada-produto/index.css" />
     <link rel="stylesheet" href="../../assets/css/components/navbar.css">
